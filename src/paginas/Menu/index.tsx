f7277ab316e15e870a891c7b2d93d2539b3b72c6
gracione <gracione2017@gmail.com>
2022-06-11 08:53:01 -0300

    atualizando

--- src/paginas/Menu/index.tsx
+++ src/paginas/Menu/index.tsx
@@ -8,19 +8,30 @@ import api from "../../services/api";
 
 export default function Menu() {
   const [token] = useState(localStorage.getItem("token"));
+  const [taskList, setTaskList] = useState([]);
 
   const history = useNavigate();
-  console.log(token);
   useEffect(() => {
-    api.get("me", {
+    api
+      .get("me", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
+      .then((response) => {
+        if (response.data.status && response.data.status === (401 || 498)) {
+          localStorage.clear();
+          history("/");
+        } else {
+          setTaskList(response.data.data);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }, [token]);
 
-  if (token == null) {
-    console.log(token);
+  if (token === "" || token === null) {
     history("/");
   }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Logon from './paginas/Autenticacao/Login/index';
 import Home from './paginas/Home/index';
-import Menu from './paginas/Menu/index';
 import Registrar from './paginas/Autenticacao/Registrar/index';
 import Funcionarios from './paginas/Funcionarios';
 
