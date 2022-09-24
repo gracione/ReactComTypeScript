71b58afbd247603d6189d69b797e0376618f60bd
gracione <gracione2017@gmail.com>
2022-09-24 12:50:58 -0300

    adicionando

--- src/paginas/Tratamentos/inserir.tsx
+++ src/paginas/Tratamentos/inserir.tsx
@@ -40,103 +40,99 @@ export default function InserirTratamento() {
   });
 
   const n = 1;
-    const [matrix, setMatrix] = useState(Array.from({length: n},()=> Array.from({length: n}, () => null)));
-  
-    const handleChange = (row:any, column:any, event:any) => {
-      let copy:any = [...matrix];
-      copy[row][column] = +event.target.value;
-      copy[row].push([]);
-      setMatrix(copy);
-  
-      console.log(matrix);
-    };
-    const adicionarLinha = (row:any) =>{
-      let copy:any = [...matrix];
-      copy[row].push([]);
-      setMatrix(copy);
-    }
-    const adicionarColuna = (row:any) =>{
-      let copy:any = [...matrix];
-      copy.push([]);
-      setMatrix(copy);
-    }
-    return (
-      <Container>
-        <Menu></Menu>
-        <Header>
-          <Conteudo>
-            <form onSubmit={inserir}>
-              <h2 >Adicionar Tratamento</h2>
-              <div>
-                <input
-                  placeholder="Tratamento"
-                  value={tratamento}
-                  onChange={e => setTratamento(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <input
-                  placeholder="Hora"
-                  value={horas}
-                  type="number"
-                  onChange={e => setHoras(e.target.value)}
-                  required
-                />
-                <input
-                  placeholder="Minutos"
-                  value={minutos}
-                  type="number"
-                  onChange={e => setMinutos(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <select
-                  onChange={e => setIdProfissao(e.target.value)}
-                  required
-                >
-                  <option value={0}>Escolha a Profissão</option>
-                  {optionProfissoes}
-                </select>
-              </div>
+  const [matrix, setMatrix] = useState(Array.from({ length: n }, () => Array.from({ length: n }, () => null)));
 
+  const handleChange = (row: any, column: any, event: any) => {
+    let copy: any = [...matrix];
+    copy[row][column] = +event.target.value;
+    copy[row].push([]);
+    setMatrix(copy);
 
-              <div className="sheet">
-                <table>
-                  <tbody>
-                    {matrix.map((row:any, rowIndex:any) => (
-                      <div key={rowIndex}>
-                        {row.map((column:any, columnIndex:any) => (
-                          <div className="display-flex" key={columnIndex}>
-                            <input
-                              type="number"
-                              onChange={e => handleChange(rowIndex, columnIndex, e)}
-                            />
-                            <input
-                              type="number"
-                              onChange={e => handleChange(rowIndex, columnIndex, e)}
-                            />
-                          </div>
-                        ))}
-                        <td>
-
-                        <input onClick={() => adicionarLinha(rowIndex)} placeholder="add" />
-                        <input onClick={() => adicionarColuna(rowIndex)} placeholder="coluna" />
-                        </td>
-                      </div>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-
-
-
-              <button type="submit">Salvar</button>
-            </form>
-
-          </Conteudo>
-        </Header>
-      </Container>
-    );
+    console.log(matrix);
+  };
+  const adicionarLinha = (row: any) => {
+    let copy: any = [...matrix];
+    copy[row].push([]);
+    setMatrix(copy);
   }
+  const adicionarColuna = () => {
+    let copy: any = [...matrix];
+    copy.push([]);
+    setMatrix(copy);
+  }
+  return (
+    <Container>
+      <Menu></Menu>
+      <Header>
+        <Conteudo>
+          <form onSubmit={inserir}>
+            <h2 >Adicionar Tratamento</h2>
+            <div>
+              <input
+                placeholder="Tratamento"
+                value={tratamento}
+                onChange={e => setTratamento(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <input
+                placeholder="Hora"
+                value={horas}
+                type="number"
+                onChange={e => setHoras(e.target.value)}
+                required
+              />
+              <input
+                placeholder="Minutos"
+                value={minutos}
+                type="number"
+                onChange={e => setMinutos(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <select
+                onChange={e => setIdProfissao(e.target.value)}
+                required
+              >
+                <option value={0}>Escolha a Profissão</option>
+                {optionProfissoes}
+              </select>
+            </div>
+
+
+            <div className="border">
+              {matrix.map((row: any, rowIndex: any) => (
+                <div key={rowIndex}>
+                  {row.map((column: any, columnIndex: any) => (
+                    <div className="display-flex" key={columnIndex}>
+                      <input
+                        type="number"
+                        onChange={e => handleChange(rowIndex, columnIndex, e)}
+                      />
+                      <input
+                        type="number"
+                        onChange={e => handleChange(rowIndex, columnIndex, e)}
+                      />
+                    </div>
+                  ))}
+                  <div>
+
+                  <div onClick={() => adicionarLinha(rowIndex)}>add</div>
+                  <div onClick={() => adicionarColuna()}>coluna</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+
+
+            <button type="submit">Salvar</button>
+          </form>
+
+        </Conteudo>
+      </Header>
+    </Container>
+  );
+}
